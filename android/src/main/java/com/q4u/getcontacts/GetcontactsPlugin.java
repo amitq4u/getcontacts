68ddee39138f39fcbdd2d79c516803c0cf727abb
Amit Kumar <amitq4u@gmail.com>
2021-12-03 16:21:02 +0530

    Contacts from phone plugin running in flutter

--- android/src/main/java/com/q4u/getcontacts/GetcontactsPlugin.java
+++ android/src/main/java/com/q4u/getcontacts/GetcontactsPlugin.java
@@ -2,35 +2,47 @@ package com.q4u.getcontacts;
 
 import static io.flutter.util.ViewUtils.getActivity;
 
+import android.app.Activity;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Build;
 import android.provider.ContactsContract;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import io.flutter.Log;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
 /** GetcontactsPlugin */
-public class GetcontactsPlugin implements FlutterPlugin, MethodCallHandler  {
+public class GetcontactsPlugin implements FlutterPlugin, MethodCallHandler ,ActivityAware{
+  public static final int REQUEST_READ_CONTACTS = 79;
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
+  private Context context;
+  private Activity activity;
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "getcontactsAmit");
     channel.setMethodCallHandler(this);
+    context = flutterPluginBinding.getApplicationContext();
+
   }
 
   @Override
@@ -39,12 +51,16 @@ public class GetcontactsPlugin implements FlutterPlugin, MethodCallHandler  {
       result.success("Android " + Build.ID);
     }
     else if (call.method.equals("getContactList")) {
-      //ArrayList<String> nList = getAllContacts();
-      ArrayList<String> nList = new ArrayList<>();;
+      Log.e("mPlugin","Pass");
+      //Log.e("mPlugin",getAllContacts().toString());
+      ArrayList<String> nList = getAllContacts();
+      /*
+      ArrayList<String> nList = new ArrayList<>();
       nList.add("Amit");
       nList.add("Sumit");
       nList.add("Aman");
       nList.add("Sam");
+       */
       result.success(nList);
     }
     else {
@@ -56,10 +72,12 @@ public class GetcontactsPlugin implements FlutterPlugin, MethodCallHandler  {
   public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
     channel.setMethodCallHandler(null);
   }
-/*
+
   public ArrayList getAllContacts() {
     ArrayList<String> nameList = new ArrayList<>();
-    ContentResolver cr = getContentResolver();
+    //Log.e("mPlugin",context.toString());
+    //Log.e("mPlugin",activity.toString());
+    ContentResolver cr = context.getContentResolver();
     Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
             null, null, null, null);
     if ((cur != null ? cur.getCount() : 0) > 0) {
@@ -89,5 +107,26 @@ public class GetcontactsPlugin implements FlutterPlugin, MethodCallHandler  {
     return nameList;
   }
 
- */
+
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+    Log.e("mPlugin","Activity Binded");
+    Log.e("mPlugin",binding.getActivity().toString());
+
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+
+  }
 }
